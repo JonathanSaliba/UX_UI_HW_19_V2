2b7f1bf326e518b9064d5f168ba693867eaa5bfd
JonathanSaliba <Jonathan.saliba@ontariotechu.net>
2022-05-22 21:35:44 -0400

    Finished Testimonial seection and styling. Added Contact me section and forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperProfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -100,7 +100,6 @@ let swiper = new Swiper('.portfolio__container', {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -108,7 +107,22 @@ let swiper = new Swiper('.portfolio__container', {
   });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
